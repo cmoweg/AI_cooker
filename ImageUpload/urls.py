b59d995ebe6869118b0ff1c6ce4136b49faf04f8
cmoweg <cmoweg@gmail.com>
2020-12-22 18:26:48 +0900

    complete image upload

--- ImageUpload/urls.py
+++ ImageUpload/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.todayFood, name='todayFood'),
+    path('detail/<int:pk>', views.detail, name='detail'),
+    path('upload/', views.upload, name='upload'),
 ]
--- ImageUpload/views.py
+++ ImageUpload/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import FoodImage
 # Create your views here.
 
@@ -13,10 +13,18 @@ def upload(request):
     # food.pub_date = timezone.datetime.now() model func으로 대체
     food.user = request.user
     # # # Ai model
+    food.photo = request.FILES['imgs']
     food.imageIdx = 0
     # # #
     food.save()
     # name 속성이 imgs인 input 태그로부터 받은 파일들을 반복문을 통해 하나씩 가져온다
-    return redirect('/detail/' + str(food.id))
+    return redirect('detail', food.id)
     # else:
     #     return render(request, 'new.html')
+
+
+def detail(request, pk):
+    context = {
+        'food': FoodImage.objects.get(pk=pk)
+    }
+    return render(request, 'ImageUpload/FoodDetail.html', context)
